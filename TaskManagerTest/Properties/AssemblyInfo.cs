4827d723865593755c959d2fdd404cd260ecd318
Renattele <61118261+renattele@users.noreply.github.com>
2026-05-14 13:10:16 +0000

    init

---
+++ TaskManagerTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TaskManagerTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TaskManagerTest")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("56a1894c-603d-493d-bb28-238284c46355")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TaskManagerTest/UnitTest1.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using FlaUI.UIA3;
+using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Input;
+using FlaUI.Core.WindowsAPI;
+
+namespace TaskManagerTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestTaskManager()
+        {
+            Process.Start("mspaint.exe");
+            Thread.Sleep(5000);
+
+            Assert.IsTrue(IsProcessRunning("mspaint"), "Paint not running");
+
+            Process.Start("taskmgr.exe");
+            Thread.Sleep(5000);
+
+            using (var automation = new UIA3Automation())
+            {
+                var desktop = automation.GetDesktop();
+                var taskManagerProcess = Process.GetProcessesByName("Taskmgr").First();
+                var taskManager = automation.FromHandle(taskManagerProcess.MainWindowHandle).AsWindow();
+
+                taskManager.Focus();
+
+                Thread.Sleep(1000);
+
+                Keyboard.TypeSimultaneously(VirtualKeyShort.CONTROL, VirtualKeyShort.KEY_F);
+
+                Thread.Sleep(1000);
+
+                Keyboard.Type("paint");
+                Thread.Sleep(5000);
+
+                Keyboard.Press(VirtualKeyShort.DOWN);
+                Thread.Sleep(5000);
+
+                Keyboard.Press(VirtualKeyShort.LMENU);
+                Keyboard.Press(VirtualKeyShort.KEY_E);
+                Keyboard.Release(VirtualKeyShort.LMENU);
+                Keyboard.Release(VirtualKeyShort.KEY_E);
+
+                Thread.Sleep(3000);
+                Assert.IsFalse(IsProcessRunning("mspaint"), "Paint is not killed");
+
+                taskManager.Close();
+            }
+        }
+
+
+        static bool IsProcessRunning(string name)
+        {
+            return Process.GetProcessesByName(name).Any();
+        }
+    }
+}
